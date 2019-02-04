ce95235f0f7ccb8ed87cbd398c79203c9037d6ca
Eduardo Finotti <eduardofinotti@Eduardos-MacBook-Pro.local>
2019-02-04 10:33:01 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# `html-reporter-mobile-test`
+
+> Functions to maker HTML reporter to mobile tests
+
+## Installation
+
+You can install it with *npm* .
+
+Try:
+
+- *npm*: `npm install html-reporter-mobile-test`
+
+## Usage
+
+```js
+const htmlMaker = require('html-reporter-mobile-test');
+var html=null;
+
+html = html + htmlMaker.makeHtmlHeader(icon, 'iOS', '23/10/2019', 'iPhone 7');
+html = html + htmlMaker.makeHtmlFooter(4, 2, '3 minutes', '2 minutes');
+```
+
+Now, with `htmlMaker` you can use some functions:
+
+- `makeHtmlHeader(icon, platform, date, device)`
+- `makeHtmlFooter(testPassed, testFailed, totalDuration, duration)`
+- `makeHtmlTestFail(title, err)`
+- `makeHtmlTestSuccess(title)`
+- `makeHtmlFeature(suite)`
+
+
+### `makeHtmlHeader(icon, platform, date, device)`
+
+> Create header of html reporter.
+
+#### Parameters
+
+- `bannerHeader` {* String *}: Banner to header.
+- `icon` {* String *}: Platform icon (iOS or Android).
+- `platform` {* String *}: Platform name (Android, iOS ...).
+- `date` {* String *}: The date the test was run.
+- `device` {* String *}: Which device is running the test.
+
+#### Example
+
+```js
+html = html + htmlMaker.makeHtmlHeader('https://img.icons8.com/metro/1600/mac-os.png', 'https://img.icons8.com/metro/1600/mac-os.png', 'iOS', '23/10/2019', 'iOS');
+```
+
+Returns a html.
+
+
+### `makeHtmlFooter(testPassed, testFailed, totalDuration, duration)`
+
+> Create footer of html reporter.
+
+#### Parameters
+
+- `bannerFooter` {* String *}: Banner to footer.
+- `testPassed` {* String *}: Number of tests that passed successfully.
+- `testFailed` {* String *}: Number of tests that failed.
+- `totalDuration` {* String (MILLISECOND) *}: Total duration of test execution. 
+- `duration` {* String (MILLISECOND)*}: Duration of only the stage where the app was being used.
+
+#### Example
+
+```js
+html = html + htmlMaker.makeHtmlFooter('https://img.icons8.com/metro/1600/mac-os.png', 5, 4, '5 minutes', '3 minutes');
+```
+
+Returns a html.
+
+### `makeHtmlTestFail(title, err)`
+
+> Create html when test fail.
+
+#### Parameters
+
+- `title` {* String *}: Name of test.
+- `err` {* String *}: Description off fail test.
+
+Returns a html.
+
+#### Example
+
+```js
+html = html + htmlMaker.makeHtmlTestFail('Login com user an password', 'Timeout');
+```
+
+### `makeHtmlTestSuccess(title)`
+
+> Create html when test success.
+
+#### Parameters
+
+- `title` {* String *}: Name of test.
+
+Returns a html.
+
+#### Example
+
+```js
+html = html + htmlMaker.makeHtmlTestSuccess('Login whitc user and password');
+```
+
+### `makeHtmlFeature(suite)`
+
+> Create html to each new suite.
+
+#### Parameters
+
+- `suite` {* String *}: Name of suite or feature.
+
+Returns a html.
+
+#### Example
+
+```js
+html = html + htmlMaker.makeHtmlFeature('Login';
+```
+
+## Final result
+
+https://uploaddeimagens.com.br/images/001/874/614/original/Screen_Shot_2019-02-04_at_09.01.08.png?1549278244
+https://uploaddeimagens.com.br/images/001/874/616/original/Screen_Shot_2019-02-04_at_09.01.22.png?1549278278
+
+## License
+
+MIT &copy; Eduardo Finotti
---
+++ index.js
@@ -0,0 +1,103 @@
+const prettyMs = require('pretty-ms');
+
+module.exports = {
+
+    makeHtmlHeader(bannerHeader, icone, platform, date, device){
+
+        return '<!DOCTYPE html><html style="box-sizing: inherit;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;overflow-x: '
+        +'hidden;font-family: Verdana,sans-serif;font-size: 15px;line-height: 1.5;"><meta name="viewport" content="width=device-width, '
+        +'initial-scale=1" style="box-sizing: inherit;"> <body style="box-sizing: inherit;margin: 0;font-family: Verdana,sans-serif;font-size: '
+        +'15px;line-height: 1.5;"><img src="'+bannerHeader+'" width="1000" '
+        +'height="110" style="box-sizing: inherit;border-style: none;vertical-align: middle;"><div class="w3-container" style="box-sizing: '
+        +'inherit;padding: 0.01em 16px;"> <h2 style="box-sizing: inherit;font-size: 30px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;'
+        +'font-weight: 400;margin: 10px 0;">Informações</h2> <p style="box-sizing: inherit;">Informações iniciais da execução dos testes:</p> '
+        +'<div class="w3-card-4" style="width: 155px;display: inline-block;box-sizing: inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px '
+        +'20px 0 rgba(0,0,0,0.19); border: 1px solid #b7b7c0"> <header class="w3-container w3-blue" style="box-sizing: inherit;display: '
+        +'block;padding: 0.01em 16px;color: #fff!important;background-color: #2196F3!important;"> <h4 style="display: inline-block;box-sizing: '
+        +'inherit;font-size: 20px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px 0;">Plataforma</h4> </header> '
+        +'<div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <img src="'+icone+'" width="30" height="30" style="display: '
+        +'inline-block;box-sizing: inherit;border-style: none;vertical-align: middle;"> <p style="display: inline-block;font-weight: bold;'
+        +'box-sizing: inherit;">'+platform+'</p> </div> </div> <div class="w3-card-4" style="width: 155px;display: inline-block;border: '
+        +'1px solid #b7b7c0 !important;box-sizing: inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);"> <header '
+        +'class="w3-container w3-blue" style="box-sizing: inherit;display: block;padding: 0.01em 16px;color: #fff!important;background-color: '
+        +'#2196F3!important;"> <h4 style="display: inline-block;box-sizing: inherit;font-size: 20px;font-family: &quot;Segoe UI&quot;,Arial,'
+        +'sans-serif;font-weight: 400;margin: 10px 0;">Device</h4> </header> <div class="w3-container" style="box-sizing: inherit;padding: '
+        +'0.01em 16px;"> <img src="https://cdn.icon-icons.com/icons2/936/PNG/512/mobile-phone_icon-icons.com_73484.png" width="30" height="30" '
+        +'style="display: inline-block;box-sizing: inherit;border-style: none;vertical-align: middle;"> <p style="display: inline-block;'
+        +'font-weight: bold;box-sizing: inherit;">' + device +'</p> </div> </div><div class="w3-card-4" style="width: 350px;display: inline-block;'
+        +'box-sizing: '
+        +'inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);border: 1px solid #b7b7c0"> <header class="w3-container '
+        +'w3-blue" style="box-sizing: inherit;display: block;padding: 0.01em 16px;color: #fff!important;background-color: #2196F3!important;"> '
+        +'<h4 style="display: inline-block;box-sizing: inherit;font-size: 20px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: '
+        +'400;margin: 10px 0;">Data e Hora</h4> </header> <div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <img '
+        +'src="https://img.icons8.com/metro/1600/clock.png" width="30" height="30" style="display: inline-block;box-sizing: inherit;border-style: '
+        +'none;vertical-align: middle;"> <p style="display: inline-block;font-weight: bold;box-sizing: inherit;">'+date+'</p> </div> </div> '
+        +'</div><br style="box-sizing: inherit;"><div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <h2 '
+        +'style="box-sizing: inherit;font-size: 30px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px '
+        +'0;">Results</h2></div>';
+    },
+
+    makeHtmlFooter(bannerFooter, testPassed, testFailed, totalDuration, duration){
+
+        return '<div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <h2 style="box-sizing: inherit;font-size: '
+        +'30px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px 0;">Resume</h2> <div align="center" '
+        +'class="w3-card-4" style="width: 150px;display: inline-block;box-sizing: inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px '
+        +'0 rgba(0,0,0,0.19);border: 1px solid #b7b7c0"> <header class="w3-container w3-blue" style="box-sizing: inherit;display: block;padding: '
+        +'0.01em 16px;color: #fff!important;background-color: #2196F3!important;"> <h4 style="display: inline-block;box-sizing: inherit;font-size: '
+        +'20px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px 0;">Total</h4> </header> <div class="w3-container" '
+        +'style="box-sizing: inherit;padding: 0.01em 16px;"> <p style="display: inline-block;font-weight: bold;box-sizing: '
+        +'inherit;">'+(testFailed+testPassed)+'</p> </div> </div> <div align="center" class="w3-card-4" style="width: 150px;display: '
+        +'inline-block;box-sizing: inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);border: 1px solid #b7b7c0"> '
+        +'<header class="w3-container w3-green" style="box-sizing: inherit;display: block;padding: 0.01em 16px;color: #fff!important;'
+        +'background-color: #4CAF50!important;"> <h4 style="display: inline-block;box-sizing: inherit;font-size: 20px;font-family: &quot;Segoe '
+        +'UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px 0;">Success</h4> </header> <div class="w3-container" style="box-sizing: '
+        +'inherit;padding: 0.01em 16px;"> <p style="display: inline-block;font-weight: bold;box-sizing: inherit;">'+testPassed+'</p> </div> '
+        +'</div> <div align="center" class="w3-card-4" style="width: 150px;display: inline-block;box-sizing: inherit;box-shadow: 0 4px 10px 0 '
+        +'rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);border: 1px solid #b7b7c0"> <header class="w3-container w3-red" style="box-sizing: '
+        +'inherit;display: block;padding: 0.01em 16px;color: #fff!important;background-color: #f44336!important;"> <h4 style="display: '
+        +'inline-block;box-sizing: inherit;font-size: 20px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: '
+        +'10px 0;">Fail</h4> </header> <div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <p style="display: '
+        +'inline-block;font-weight: bold;box-sizing: inherit;">'+testFailed+'</p> </div> </div> <div align="center" class="w3-card-4" '
+        +'style="width: 250px;display: inline-block;box-sizing: inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);'
+        +'border: 1px solid #b7b7c0"> <header class="w3-container w3-blue" style="box-sizing: inherit;display: block;padding: 0.01em '
+        +'16px;color: #fff!important;background-color: #2196F3!important;"> <h4 style="display: inline-block;box-sizing: inherit;font-size: '
+        +'20px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px 0;">Total Time</h4> </header> '
+        +'<div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <p style="display: inline-block;font-weight: '
+        +'bold;box-sizing: inherit;">'+prettyMs(totalDuration, {verbose: true})+'</p> </div> </div><div align="center" class="w3-card-4" '
+        +'style="width: 250px;display: inline-block;border: 1px solid #b7b7c0;box-sizing: inherit;box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 '
+        +'4px 20px 0 rgba(0,0,0,0.19);"> <header class="w3-container w3-blue" style="box-sizing: inherit;display: block;padding: 0.01em '
+        +'16px;color: #fff!important;background-color: #2196F3!important;"> <h4 style="display: inline-block;box-sizing: inherit;font-size: '
+        +'20px;font-family: &quot;Segoe UI&quot;,Arial,sans-serif;font-weight: 400;margin: 10px 0;">Tests Time</h4> </header> '
+        +'<div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"> <p style="display: inline-block;font-weight: '
+        +'bold;box-sizing: inherit;">'+prettyMs(duration, {verbose: true})+'</p> </div> </div></div><br style="box-sizing: inherit;"><br '
+        +'style="box-sizing: inherit;"><img src="'+bannerFooter+'" '
+        +'width="1000" height="60" style="box-sizing: inherit;border-style: none;vertical-align: middle;"></body></html>'
+    },
+
+    makeHtmlTestFail(title, err){
+
+        return '<div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"><div class="w3-panel w3-leftbar w3-border-red '
+        +'w3-pale-red" style="box-sizing: inherit;padding: 0.01em 16px;margin-top: 16px;margin-bottom: 16px;border-left: 6px solid '
+        +'#ccc!important;color: #000!important;background-color: #ffdddd!important;border-color: #f44336!important;"> <p style="display: '
+        +'inline-block;box-sizing: inherit;">'+ title+'</p> <p align="left" style="font-weight: bold;box-sizing: inherit;">Error: '
+        +'<i style="box-sizing: inherit;">'+err+'</i></p> </div> </div>';
+    },
+
+    makeHtmlTestSuccess(title){
+
+        return '<div class="w3-container" style="box-sizing: inherit;padding: 0.01em 16px;"><div class="w3-panel w3-leftbar '
+        +'w3-border-green w3-pale-green" style="box-sizing: inherit;padding: 0.01em 16px;margin-top: 16px;margin-bottom: 16px;border-left: '
+        +'6px solid #ccc!important;color: #000!important;background-color: #ddffdd!important;border-color: #4CAF50!important;"> '
+        +'<p style="display: inline-block;box-sizing: inherit;">'+title+'</p> </div> </div>';
+    },
+
+    makeHtmlFeature(suite){
+
+        return '<div class="w3-panel w3-topbar w3-bottombar w3-border-blue w3-pale-blue" style="box-sizing:inherit;padding-top:'
+        +'0.01em;padding-bottom:0.01em;padding-right:16px;padding-left:16px;margin-top:16px;margin-bottom:16px;border-top-width:6px '
+        +'!important;border-top-style:solid !important;border-top-color:#ccc !important;border-bottom-width:6px !important;border-bottom-style:'
+        +'solid !important;border-bottom-color:#ccc !important;color:#000!important;background-color:#ddffff!important;border-color:#2196F3'
+        +'!important;" > <p style="box-sizing:inherit;font-weight:bold;" >'+ suite+'</p></div>';
+    },
+
+}
